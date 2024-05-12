0ce98ee8c7a38d122ce580ce69fcfddb58ab073d
novaardiansyah <novaardiansyah78@gmail.com>
2024-05-12 09:03:05 +0700

    Add widgets to resource pages

--- app/Filament/Resources/PostResource/Pages/CreatePost.php
+++ app/Filament/Resources/PostResource/Pages/CreatePost.php
@@ -5,8 +5,16 @@ namespace App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreatePost extends CreateRecord
 {
-    protected static string $resource = PostResource::class;
+  protected static string $resource = PostResource::class;
+
+  protected function handleRecordCreation(array $data): Model
+  {
+    $record = static::getModel()::create($data);
+    $record->users()->attach(auth()->id());
+    return $record;
+  }
 }
